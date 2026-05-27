d6bded74f9e28d3a2a26ff4cc26a86bb21673365
genMira <{weilianzhuantongyin@gmail.com}>
2026-05-27 18:11:50 +0900

    公開用設定の修正

--- main.go
+++ main.go
@@ -52,7 +52,7 @@ func main(){
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{
 			"http://localhost:8080",
-			"https://genmira.github.io/",
+			"https://genmira.github.io",
 		},
 		AllowMethods: []string{
       echo.GET, 
